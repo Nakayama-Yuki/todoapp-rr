cb68a6fc409bfd8117df0c565f29dc4c84b15d6c
yuki <yuki.for.neutral@gmail.com>
2026-01-19 13:23:36 +0900

    refactor: jsインポートを削除し、eslint設定を簡素化

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
-import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 export default defineConfig(
-  js.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
